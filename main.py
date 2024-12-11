b58a148a0e3c2d8f3ead23ceff4ef12ec8347fa6
Clarissa Lobo <139502710+ClarissaLobo@users.noreply.github.com>
2024-12-11 11:38:43 -0500

    Model and Callbacks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Chicken import logger
 from Chicken.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from Chicken.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from Chicken.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 # from Chicken.pipeline.stage_03_training import ModelTrainingPipeline
 # from Chicken.pipeline.stage_04_evaluation import EvaluationPipeline
 
@@ -10,6 +10,18 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
